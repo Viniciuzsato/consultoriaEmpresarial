b1f308dd2bdf59166717c1e67da889b63ccd76d4
Vinicius Sato <vinicius.sato@outlook.com>
2023-05-23 20:47:50 -0300

    Adicionando funcionalidade de enviar email

--- app/src/main/java/com/example/navigationdrawer/MainActivity.java
+++ app/src/main/java/com/example/navigationdrawer/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.navigationdrawer;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
@@ -32,8 +34,7 @@ public class MainActivity extends AppCompatActivity {
         binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                enviarEmail();
             }
         });
         DrawerLayout drawer = binding.drawerLayout;
@@ -49,6 +50,18 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+    }
+
+    public void enviarEmail(){
+        Intent intent = new Intent(Intent.ACTION_SEND);
+
+        intent.putExtra( Intent.EXTRA_EMAIL, new String[] {"atendimento@consultoria.com.br"});
+        intent.putExtra( Intent.EXTRA_SUBJECT, "Contato pelo App");
+        intent.putExtra( Intent.EXTRA_TEXT, "Mensagem automática");
+
+        intent.setType("message/rfc822");
+
+        startActivity( Intent.createChooser( intent, "Compartilhar") );
     }
 
 
