b15a86e93dd09de3d825daacb5676d9dd501e8f4
Vinicius Sato <vinicius.sato@outlook.com>
2023-05-23 21:17:32 -0300

    Criando a pagina Sobre da empresa

--- app/src/main/java/com/example/navigationdrawer/ui/sobre/SobreFragment.java
+++ app/src/main/java/com/example/navigationdrawer/ui/sobre/SobreFragment.java
@@ -10,6 +10,8 @@ import android.view.ViewGroup;
 
 import com.example.navigationdrawer.R;
 
+import mehdi.sakout.aboutpage.AboutPage;
+
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link SobreFragment#newInstance} factory method to
@@ -60,7 +62,26 @@ public class SobreFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sobre, container, false);
+
+        String descricao = "A ATM Consultoria tem como missão apoiar organizações que desejam alcancar o " +
+                "sucesso atráves da excelência em gestão e da busca pela qualidade.";
+
+        return new AboutPage(getActivity())
+                .setImage(R.drawable.logo)
+                .setDescription( descricao )
+
+                .addGroup("Entre em contato")
+                .addEmail("atendimento@atmconsultoria.com.br", "Envie um e-mail")
+                .addWebsite("www.google.com/", "Acesse nosso site.")
+
+                .addGroup("Redes Sociais")
+                .addFacebook("atmconsultoria", "Facebook")
+                .addTwitter("atmconsultoria", "Twitter")
+                .addYoutube("atmconsultoria", "Youtube")
+                .addGitHub("atmconsultoria", "GitHub")
+                .addInstagram("atmconsultoria", "Instagram")
+                .create();
+
+        //return inflater.inflate(R.layout.fragment_sobre, container, false);
     }
 }
